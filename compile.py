84c5a1865c4a04e97e71a718a4a0428302e81249
Josh Stockin <joshstockin@gmail.com>
2019-11-11 15:35:03 -0600

    Formatting, error page

--- compile.py
+++ compile.py
@@ -11,10 +11,10 @@ def readfile(filename):
 			file.close()
 			return s
 	except FileNotFoundError:
-		print(filename + " not found. exitting")
+		print(filename + " not found. exiting")
 		exit(1)
 	except:
-		print("can't open " + filename + " for reading. exitting")
+		print("can't open " + filename + " for reading. exiting")
 		exit(1)
 
 def writefile(filename, text):
@@ -23,17 +23,32 @@ def writefile(filename, text):
 			file.write(text)
 			file.close()
 	except:
-		print("can't open " + filename + " for writing. exitting")
+		print("can't open " + filename + " for writing. exiting")
 		exit(1)
+
+routemaps = {}
+def routemap(route, file):
+	routemaps[route] = file
 
 def main():
 	config = json.loads(readfile("config.json"))
+
+	# Index
+	print("creating landing")
+	landing = readfile(config["templates"]["landing"])
+	file = os.path.join(out_path, "index.html")
+	writefile(file, landing)
+	routemap("/", file)
+
+	# /blog*
 	print("creating blog articles")
 	listings = ""
 	article_listing_template = readfile(config["templates"]["blog-archive-listing"])
 	article_template = readfile(config["templates"]["blog-article"])
 	blog_css = readfile(config["templates"]["blog-css"])
+
 	for article in config["articles"]:
+		# Create article
 		print("creating article \"" + article["title"] + "\"")
 		articlehtml = "" + article_template
 		articlehtml = articlehtml.replace("$css", blog_css)
@@ -42,25 +57,41 @@ def main():
 		articlehtml = articlehtml.replace("$banner", article["banner"])
 		articlehtml = articlehtml.replace("$content", readfile(article["content"]))
 		articlehtml = articlehtml.replace("$copyright", config["copyright"])
-		link = article["title"].lower().replace(" ", "-")+".html"
-		articlehtml = articlehtml.replace("$permalink", link)
-		writefile(os.path.join(out_path, link), articlehtml)
+		file = os.path.join(out_path, "blog-"+article["title"].lower().replace(" ", "-")+".html")
+		path = "/blog/"+article["title"].lower().replace(" ", "-")
+		articlehtml = articlehtml.replace("$permalink", path)
+		writefile(file, articlehtml)
+		routemap(path, file)
+
+		# Update archive listings
 		listinghtml = "" + article_listing_template
 		listinghtml = listinghtml.replace("$title", article["title"])
 		listinghtml = listinghtml.replace("$date", article["date"])
 		listinghtml = listinghtml.replace("$banner", article["banner"])
 		listinghtml = listinghtml.replace("$summary", article["summary"])
-		listinghtml = listinghtml.replace("$permalink", link)
+		listinghtml = listinghtml.replace("$permalink", path)
 		listings = listinghtml + listings
+
+	# Blog archive
 	print("creating blog archive")
 	archive_template = readfile(config["templates"]["blog-archive"])
 	archive_template = archive_template.replace("$css", blog_css)
 	archive_template = archive_template.replace("$articles", listings)
 	archive_template = archive_template.replace("$copyright", config["copyright"])
-	writefile(os.path.join(out_path, "blog.html"), archive_template)
-	print("creating landing")
-	landing = readfile(config["templates"]["landing"])
-	writefile(os.path.join(out_path, "index.html"), landing)
+	file = os.path.join(out_path, "blog.html")
+	writefile(file, archive_template)
+	routemap("/blog", file)
+
+	# Error 404
+	print("creating 404 error page")
+	e404 = readfile(config["templates"]["404"])
+	file = os.path.join(out_path, "error-404.html")
+	writefile(file, e404)
+
+	# Routemap config
+	print("Routemap:")
+	for map in routemaps:
+		print(map)
 
 if __name__ == "__main__":
 	if len(sys.argv) < 2:
@@ -69,7 +100,7 @@ if __name__ == "__main__":
 	folder_out = sys.argv[1]
 	print("compile.py starting")
 	if not os.path.isdir(folder_out):
-		print(folder_out + " is not a valid folder location. exitting")
+		print(folder_out + " is not a valid folder location. exiting")
 		exit(1)
 	out_path = os.path.abspath(folder_out)
 	print("output set to " + out_path)

603fa7dac650fb77cba17e247e885f7aa542fcc4
Josh Stockin <joshstockin@gmail.com>
2020-01-04 16:21:24 -0600

    Remove / route function from express.js for static serving

--- gitpager/index.js
+++ gitpager/index.js
@@ -5,9 +5,5 @@ const app = express();
 const port = 8080;
 
 app.use(express.static(path.resolve(__dirname, "static")))
-let indexFile = path.resolve(__dirname, "index.html")
-app.get("/", function (req, res) {
-	res.sendFile(indexFile);
-});
 
 app.listen(port);

138dce2f8fae12bc04b21bb6ad9f92607611cbb5
Josh Stockin <joshstockin@gmail.com>
2020-03-22 14:34:43 -0500

    Move root site template engine to separate folder
    Places all resources and engine files in a new root/ folder instead of
    keeping them in the project root. As this project has expanded beyond
    the root engine, this should keep things more orderly.
    
    TODO: Bring README.md up to date
    TODO: Add markdown->HTML rendering capabilities for blog compilation

--- root/compile.py
+++ root/compile.py
@@ -44,10 +44,10 @@ def empty_dir(path):
 routemaps = {}
 sitemap = """<?xml version="1.0" encoding="UTF-8"?>
 <urlset
-	  xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
-	  xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-	  xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9
-		http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">"""
+	xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
+	xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+	xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9
+	http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">"""
 def routemap(route, priority=0.5):
 	routemaps[route] = True
 	global sitemap

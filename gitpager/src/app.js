460da80920ea2d23dba77a574d855ec4b7e419d8
Josh Stockin <josh@joshstock.in>
2020-11-30 16:21:38 -0600

    Remove gitpager directory

--- gitpager/src/app.js
+++
@@ -1,3 +0,0 @@
-const path = require("path");
-const config = require("./config");
-const server = require("./server");
--- gitpager/src/pathFix.js
+++
@@ -1,6 +0,0 @@
-const path = require('path');
-const rootDir = path.resolve(__dirname, "..");
-
-module.exports = function(local) {
-	return path.resolve(rootDir, local);
-}
